a3d0a8dc95f60848a82d47a7e0d4903907cd27d6
fathin210 <fathin210@gmail.com>
2021-06-08 19:51:40 +0700

    update nav

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -5,7 +5,7 @@ function Nav() {
   const router = useRouter();
   return (
     <nav className="relative">
-      <div className="absolute z-10 top-0 left-0 bg-gradient-to-r from-[#06202A] h-10 w-1/8" />
+      <div className="absolute z-10 top-0 left-0 bg-gradient-to-r from-[#06202A] h-10 w-1/12 sm:w-0" />
       <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 overflow-x-scroll scrollbar-hide">
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
@@ -17,7 +17,7 @@ function Nav() {
           </h2>
         ))}
       </div>
-      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12 sm:w-0" />
     </nav>
   );
 }
